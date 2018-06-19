aea8b114f7de90aee6503476f7d84debeaf1e6e2
Clervens Volcy <clervens.volcy@inovva.com>
2018-06-19 15:56:29 -0400

    update navbar and remove mongoose since react is a frontend project

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,6 +3,8 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import Avatar from '@material-ui/core/Avatar';
+import LinkedInIcon from 'mdi-material-ui/Linkedin'
+import IconButton from '@material-ui/core/IconButton';
 
 import './styles.css'
 
@@ -13,8 +15,17 @@ const NavBar = () => {
             <Toolbar>
                 <Avatar alt="Clervens Volcy" src="/images/logo-100.png" />
                 <Typography variant="title" color="inherit" className="brand">
-                Clervens Volcy
+                Clervens<br />Volcy
                 </Typography>
+                <div>
+                    <IconButton
+                        href={"https://www.linkedin.com/in/clervens-volcy-5522a670"} //{mySettings.socials.linkedin"}
+                        target="_blank"
+                        color="inherit"
+                    >
+                        <LinkedInIcon />
+                    </IconButton>
+                </div>
             </Toolbar>
         </AppBar>
         </div>
