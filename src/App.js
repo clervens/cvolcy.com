ea13d32aef066552d8419e591fad441307986ffe
Clervens Volcy <clervens.volcy@gmail.com>
2018-08-09 21:07:19 -0400

    Create the basis of the new website

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
-import NavBar from './components/Navbar'
+
+import Avatar from '@material-ui/core/Avatar';
+
+import Header from "./components/Header";
+import HeaderLinks from "./components/HeaderLinks";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <NavBar />
+      <Header
+        color="transparent"
+        brand="Clervens Volcy"
+        rightLinks={<HeaderLinks />}
+        leftLinks={this.getHeaderLinks()}
+        fixed
+        changeColorOnScroll={{
+          height: 400,
+          color: "primary"
+        }} />
+    );
+  }
+
+  getHeaderLinks() {
+    return (
+      <Avatar alt="Clervens Volcy" src="/images/logo-100.png" />
     );
   }
 }
---
+++ src/components/HeaderLinks/index.jsx
@@ -0,0 +1,53 @@
+/*eslint-disable*/
+import React from "react";
+
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Tooltip from "@material-ui/core/Tooltip";
+import LinkedInIcon from 'mdi-material-ui/Linkedin'
+import IconButton from '@material-ui/core/IconButton';
+
+// @material-ui/icons
+import { Apps, CloudDownload } from "@material-ui/icons";
+
+// core components
+// import CustomDropdown from "components/CustomDropdown/CustomDropdown.jsx";
+// import Button from "components/CustomButtons/Button.jsx";
+
+import headerLinksStyle from "./styles.jsx";
+
+function HeaderLinks({ ...props }) {
+  const { classes } = props;
+  return (
+    <List className={classes.list}>
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          id="linkedin-tooltip"
+          title="Add me on LinkedIn"
+          placement={window.innerWidth > 959 ? "top" : "left"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          {/* <Button
+            color="transparent"
+            href="https://www.instagram.com/CreativeTimOfficial"
+            target="_blank"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + " fab fa-instagram"} />
+          </Button> */}
+          <IconButton
+            href={"https://www.linkedin.com/in/clervens-volcy-5522a670"}
+            target="_blank"
+            color="inherit"
+          >
+            <LinkedInIcon />
+          </IconButton>
+        </Tooltip>
+      </ListItem>
+    </List>
+  );
+}
+
+export default withStyles(headerLinksStyle)(HeaderLinks);
